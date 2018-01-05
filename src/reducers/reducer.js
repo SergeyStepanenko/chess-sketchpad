889dbdba1d1624fb3cf94fd643ca6c642af984ea
Sergey <groverfmx@gmail.com>
2018-01-06 00:35:05 +0300

    ref

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -9,19 +9,19 @@ const reducer = (state = fromJS(STATE), action) => {
 		return state.set('onDrag', false).set('activeFigure', null);
 	case 'UPDATE_CELL_ADD':
 		return state
-			.updateIn([ 'cells', action.payload.id, 'figureId' ], () => action.payload.activeFigure)
-			.updateIn([ 'cells', action.payload.id, 'empty' ], () => false)
+			.setIn([ 'cells', action.payload.id, 'figureId' ],  action.payload.activeFigure)
+			.setIn([ 'cells', action.payload.id, 'empty' ], false)
 			.set('onDrag', false);
 	case 'UPDATE_CELL_REMOVE':
 		return state
-			.updateIn([ 'cells', action.payload.id, 'figureId' ], () => null)
-			.updateIn([ 'cells', action.payload.id, 'empty' ], () => true)
+			.setIn([ 'cells', action.payload.id, 'figureId' ], null)
+			.setIn([ 'cells', action.payload.id, 'empty' ], true)
 			.set('activeFigure', null)
 			.set('onDrag', false);
 	case 'REMOVE_KING':
-		return state.updateIn([ 'figures', action.payload, 'quantity' ], () => 0);
+		return state.setIn([ 'figures', action.payload, 'quantity' ], 0);
 	case 'RESTORE_KING':
-		return state.updateIn([ 'figures', action.payload, 'quantity' ], () => 1);
+		return state.setIn([ 'figures', action.payload, 'quantity' ], 1);
 	default:
 		return state;
 	}
